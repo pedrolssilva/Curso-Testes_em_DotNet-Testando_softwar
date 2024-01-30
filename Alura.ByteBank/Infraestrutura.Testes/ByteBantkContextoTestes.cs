bd1816dd561abdd2fb280d36c47c997ac14d005b
Pedro Luis <pedrolssilva@hotmail.com>
2024-01-29 21:06:02 -0300

    Created test project to test infrastructure layer

---
+++ Alura.ByteBank/Infraestrutura.Testes/ByteBantkContextoTestes.cs
@@ -0,0 +1,34 @@
+﻿using Alura.ByteBank.Dados.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ByteBantkContextoTestes
+    {
+        [Fact]
+        public void TestaConexaoContextoComBDMySql()
+        {
+            // Arrange
+            var contexto = new ByteBankContexto();
+            bool conectado;
+
+            // Act
+            try
+            {
+                conectado = contexto.Database.CanConnect();
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Não foi possível conectar a base de dados.");
+            }
+
+            // Assert
+            Assert.True(conectado);
+        }
+    }
+}
