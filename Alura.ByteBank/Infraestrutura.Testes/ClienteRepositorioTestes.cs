78fea8cd6061d2eb062bf8d76d12e5c2c87b59ca
Pedro Luis <pedrolssilva@hotmail.com>
2024-02-14 20:40:17 -0300

    intruduce dependency injection concept

---
+++ Alura.ByteBank/Infraestrutura.Testes/ClienteRepositorioTestes.cs
@@ -0,0 +1,32 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ClienteRepositorioTestes
+    {
+        private readonly IClienteRepositorio _repositorio;
+        public ClienteRepositorioTestes()
+        {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IClienteRepositorio, ClienteRepositorio>();
+            var provedor = servico.BuildServiceProvider();
+            _repositorio = provedor.GetService<IClienteRepositorio>();
+        }
+
+        [Fact]
+        public void TestaObterTodosClientes()
+        {
+            // Arrange
+
+            // Act
+            List<Cliente> lista = _repositorio.ObterTodos();
+
+            // Assert
+            Assert.NotNull(lista);
+            Assert.Equal(3, lista.Count);
+        }
+    }
+}
